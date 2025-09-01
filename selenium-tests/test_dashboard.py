5b48d50b831ea77d0476d46a03948fb26414b7cc
Yassine ELHANFAOUI <yelhanfaoui@dataprotect.ma>
2025-09-01 05:41:10 +0100

    update

--- selenium-tests/test_dashboard.py
+++ selenium-tests/test_dashboard.py
@@ -12,7 +12,7 @@ PASSWORD = os.getenv("TEST_PASS")
 @pytest.fixture(scope="module")
 def driver():
     options = webdriver.ChromeOptions()
-   # options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--ignore-certificate-errors")  # <-- ignore self-signed SSL
